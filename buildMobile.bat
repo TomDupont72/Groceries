e46e77e03a14c847e9a2c10c1a377df210fdac69
Dupont Tom <tomdupont72@gmail.com>
2026-02-21 17:32:27 +0100

    Clean repo

--- buildMobile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\FlowUP\Desktop\Groceries\project\mobile
-
-eas build -p android --profile preview
--- runMobileLinter.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\Users\FlowUP\Desktop\Finance\Groceries\project\mobile
-npm run lint
-pause
-npm run format
-pause
